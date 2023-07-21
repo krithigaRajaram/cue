b0e7b152e188ba22d60c1b3eaa03e0f3a86fd0dd
krithiga <krithigar2020@gmail.com>
2023-07-21 15:04:00 +0530

    Database connection established!

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.set('views',__dirname+ '/views')
 const indexRouter = require('./routes/index')
 app.set(express.static('public'))
 app.use('/',indexRouter)
-app.listen(3000,()=>{
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/cue' ,{
+    useNewUrlParser:true
+})
+const db=mongoose.connection
+db.on('error', error =>{
+    console.log("conection error!")
+})
+db.once('open',()=>{
+    console.log("connection established")
+})
+
+app.listen(3003,()=>{
     console.log("server listening!");
 })
