e7c47f205731249e1a50e30e2e6431b8dd57e073
krithiga <krithigar2020@gmail.com>
2023-07-21 14:30:35 +0530

    creation of first router

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express= require('express')
 const app=express()
+app.set('view engine','ejs')
+app.set('views',__dirname+ '/views')
+const indexRouter = require('./routes/index')
+app.set(express.static('public'))
+app.use('/',indexRouter)
 app.listen(3000,()=>{
     console.log("server listening!");
 })
